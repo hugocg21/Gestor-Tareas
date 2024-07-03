b293047b276f8964c7f8faf0a7f843850c1cd132
hugocg21 <hugocastanogonzalez@gmail.com>
2024-07-04 00:41:48 +0200

    Task-item updated

--- src/app/components/task-item/task-item.component.ts
+++ src/app/components/task-item/task-item.component.ts
@@ -1,21 +1,33 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 import { Task } from '../../models/task';
 import { differenceInDays } from 'date-fns';
 
 @Component({
   selector: 'app-task-item',
   templateUrl: './task-item.component.html',
-  styleUrls: ['./task-item.component.css']
+  styleUrls: ['./task-item.component.css'],
 })
 export class TaskItemComponent {
   @Input() task!: Task;
   @Output() toggle = new EventEmitter<void>();
   @Output() delete = new EventEmitter<void>();
 
+  getDaysRemaining(): number {
+    const dueDate = new Date(this.task.dueDate);
+    const currentDate = new Date();
+    const diffTime = dueDate.getTime() - currentDate.getTime();
+    return Math.ceil(diffTime / (1000 * 60 * 60 * 24));
+  }
+
+  isDueSoon(): boolean {
+    return this.getDaysRemaining() <= 7;
+  }
+
   getCardColor(): string {
     if (this.task.completed) {
       return 'bg-green-100';
     }
+
     const dueDate = new Date(this.task.dueDate);
     const today = new Date();
     const daysDifference = differenceInDays(dueDate, today);
@@ -25,7 +37,7 @@ export class TaskItemComponent {
     } else if (daysDifference <= 7) {
       return 'bg-yellow-100';
     } else {
-      return 'bg-white';
+      return 'bg-gray-200';
     }
   }
 }
